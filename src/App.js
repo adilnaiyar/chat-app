41c287f0f9fb59808309747b96f26609cac94e40
Adil Naiyar <adil.naiyar@gail.com>
2021-01-30 20:02:26 +0530

    added logic in messageform and loginform component creted and added the logic to validate the user and modify the app component with sound whenmessage send

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import { ChatEngine } from 'react-chat-engine';
 import ChatFeed from './components/ChatFeed/chatFeed.components';
+import LoginForm from './components/LoginForm/loginForm.components';
 import './App.css';
 
+const projectID = '5dbdc2f9-e25d-45fd-946b-7de95aebd465';
+
 const App = () => {
-  return ( 
-    <ChatEngine height="100vh" 
-      projectID="5dbdc2f9-e25d-45fd-946b-7de95aebd465"
-      userName="Adil Naiyar"
-      userSecret="adil123"
-      renderChatFeed = {(chatAppProps) => <ChatFeed {...chatAppProps} />}
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
     />
-   );
-}
+  );
+};
+
  
 export default App;
