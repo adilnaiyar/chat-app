bd38a0f9e215592fbcf540799bd378b7016047f0
Adil Naiyar <adil.naiyar@gail.com>
2021-01-30 17:23:35 +0530

    install package react-chat-engine @app-design/icons axios and modify the app component with chat engine comp of react-chat-engine

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { ChatEngine } from 'react-chat-engine';
+import './App.css';
+
+const App = () => {
+  return ( 
+    <ChatEngine height="100vh" 
+      projectID="5dbdc2f9-e25d-45fd-946b-7de95aebd465"
+      userName="Adil Naiyar"
+      userSecret="adil123"
+    />
+   );
+}
+ 
+export default App;
